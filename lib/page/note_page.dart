8fef15e92ecf36b9e12efe6bbc1d9cbaf005f799
N.S.Venkatesasn <45752506+venkatesanios@users.noreply.github.com>
2023-05-30 22:50:46 +0530

    sqflite

---
+++ lib/page/note_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:note_database/db/node_database.dart';
+import 'package:note_database/model/note.dart';
+import 'package:note_database/page/details_note_page.dart';
+import 'package:note_database/page/edit_note_page.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:note_database/widgets/note_card_widget.dart';
+
+
+class NotesPage extends StatefulWidget {
+  @override
+  _NotesPageState createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  late List<Note> notes;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    refreshNotes();
+  }
+
+  @override
+  void dispose() {
+    NotesDatabase.instance.close();
+
+    super.dispose();
+  }
+
+  Future refreshNotes() async {
+    setState(() => isLoading = true);
+
+    this.notes = await NotesDatabase.instance.readAllNotes();
+
+    setState(() => isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: const Text(
+        'Programs',
+        style: TextStyle(fontSize: 24),
+      ),
+      actions: [Icon(Icons.search), SizedBox(width: 12)],
+    ),
+    body: Center(
+      child: isLoading
+          ? CircularProgressIndicator()
+          : notes.isEmpty
+          ? const Text(
+        'No Programs available',
+        style: TextStyle(color: Colors.white, fontSize: 24),
+      )
+          : buildNotes(),
+    ),
+    floatingActionButton: FloatingActionButton(
+      backgroundColor: Colors.black,
+      child: Icon(Icons.add),
+      onPressed: () async {
+        await Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => AddEditNotePage()),
+        );
+
+        refreshNotes();
+      },
+    ),
+  );
+
+  Widget buildNotes() => StaggeredGridView.countBuilder(
+    padding: EdgeInsets.all(8),
+    itemCount: notes.length,
+    staggeredTileBuilder: (index) => StaggeredTile.fit(2),
+    crossAxisCount: 4,
+    mainAxisSpacing: 4,
+    crossAxisSpacing: 4,
+    itemBuilder: (context, index) {
+      final note = notes[index];
+
+      return GestureDetector(
+        onTap: () async {
+          await Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => NoteDetailPage(noteId: note.id!),
+          ));
+
+          refreshNotes();
+        },
+        child: NoteCardWidget(note: note, index: index),
+      );
+    },
+  );
+}
